3c237e7d198c79a970fe3569198bfbc383d6c412
Edgar Irizarry <eirizarry15@yahoo.com>
2019-04-24 18:44:34 +0000

    Automatically backed up by Learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,47 +1,26 @@
 import React from 'react'
+import Pet from './Pet'
 
-class Pet extends React.Component {
+class PetBrowser extends React.Component {
 
-  gender = () => {
-    if(this.props.pet.gender === "male"){
-      return '♂'
-    }else if(this.props.pet.gender === "female"){
-      return '♀'
-    }else{
-      return ""
-    }
-  }
 
-  adoptButton = () => {
-    if(this.props.pet.isAdopted){
-      return <button className="ui disabled button">Already adopted</button>
-    }else{
-      return <button className="ui primary button" onClick={() => this.props.onAdoptPet(this.props.pet.id)}>Adopt pet</button>
-    }
+  craftPets = () => {
+    return this.props.pets.map(petInfo => (
+        <Pet 
+          pet={petInfo}
+          onAdoptPet={this.props.onAdoptPet}
+          key={petInfo.id}
+        />
+      ))
   }
 
   render() {
     return (
-      <div className="card">
-        <div className="content">
-          <a className="header">
-            {this.gender()}
-            {this.props.pet.name}
-          </a>
-          <div className="meta">
-            <span className="date">{this.props.pet.type}</span>
-          </div>
-          <div className="description">
-            <p>Age: {this.props.pet.age}</p>
-            <p>Weight: {this.props.pet.weight}</p>
-          </div>
-        </div>
-        <div className="extra content">
-          {this.adoptButton()}
-        </div>
-      </div>
+    <div className="ui cards">
+      {this.craftPets()}
+    </div>
     )
   }
 }
 
-export default Pet
+export default PetBrowser
